33dc5552d517ccf3999f5fa076b5b28b835a1421
life_liu <liulili@ruyigou.com>
2020-10-10 16:11:50 +0800

    上传编译目录

---
+++ precache-manifest.f6a12aa3636c3817e1b150791195ae4d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2618d1a09f7a540d15c87899b365f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f2d76e94061b1979246",
+    "url": "./static/css/5.cd846464.chunk.css"
+  },
+  {
+    "revision": "ec60cc866d4e764e5033",
+    "url": "./static/css/main.ade59600.chunk.css"
+  },
+  {
+    "revision": "a61202c487807524465c",
+    "url": "./static/js/0.8d47cf2c.chunk.js"
+  },
+  {
+    "revision": "3c0465e666f76e037721",
+    "url": "./static/js/1.8acfd34b.chunk.js"
+  },
+  {
+    "revision": "6324128f314268afbd26",
+    "url": "./static/js/10.a24fa079.chunk.js"
+  },
+  {
+    "revision": "64e26b11d3bd0132a363",
+    "url": "./static/js/11.48f6c54e.chunk.js"
+  },
+  {
+    "revision": "357e95fe5830ffe4266a",
+    "url": "./static/js/12.f5aa8c1d.chunk.js"
+  },
+  {
+    "revision": "99887bb26fb8567eb5f9",
+    "url": "./static/js/2.1659cba1.chunk.js"
+  },
+  {
+    "revision": "2f2d76e94061b1979246",
+    "url": "./static/js/5.d09a9677.chunk.js"
+  },
+  {
+    "revision": "6c48000cb7e3fe060b671159dcdfb038",
+    "url": "./static/js/5.d09a9677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7512b519eb16544801c",
+    "url": "./static/js/6.778630e9.chunk.js"
+  },
+  {
+    "revision": "99cc2d1aa71a61ba1a4d",
+    "url": "./static/js/7.1ab7de78.chunk.js"
+  },
+  {
+    "revision": "91ddf3dfc203be981ae3",
+    "url": "./static/js/8.f9e7d183.chunk.js"
+  },
+  {
+    "revision": "985a3bb608aff25f2fe9",
+    "url": "./static/js/9.43f29f01.chunk.js"
+  },
+  {
+    "revision": "ec60cc866d4e764e5033",
+    "url": "./static/js/main.92fc754b.chunk.js"
+  },
+  {
+    "revision": "1c534b4cce91585eb2b3",
+    "url": "./static/js/runtime-main.bd89bb59.js"
+  },
+  {
+    "revision": "f99739d8e3914f4797ff5373183981ef",
+    "url": "./static/media/logo.f99739d8.svg"
+  }
+]);
